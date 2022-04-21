0d3002e13aa046deb4cf4bbcf96943237b305364
adammarostica <adammarostica@gmail.com>
2022-04-21 16:08:09 -0400

    Added mkdir.ogg and the ability to play it

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 const h1 = document.querySelector('h1');
 
+const mkdir = new Audio('./mkdir.ogg');
+
 h1.addEventListener('click', (e) => {
   e.target.classList.toggle('tomatoTime');
+  mkdir.play();
 });
